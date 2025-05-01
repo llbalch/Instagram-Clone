74b552f25989550c7a293df0798a1c9c1c3542d6
LaneLR <laneleerichardson@gmail.com>
2025-05-01 15:49:21 -0500

    minor js changes

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const followbtn = document.querySelector('#follow');
-let joinMV = false
 
-followbtn.addEventListener('click', () => {
-    if (joinMV === false) {
-        joinMV = true
-        alert(`You've followed Joinmultiverse!`)
-    } else if (joinMV === true) {
-    alert(`You've followed Joinmultiverse!`)
-}})
+
+
+
+
+
+const followbtn = document.querySelector("#follow");
+const messagebtn = document.querySelector("#message");
+let joinMV = false;
+
+followbtn.addEventListener("click", () => {
+  if (joinMV === false) {
+    joinMV = true;
+    alert(`You've followed Joinmultiverse!`);
+  } else if (joinMV === true) {
+    alert(`You're already following Joinmultiverse!`);
+  }
+});
+
+messagebtn.addEventListener("click", () => {
+  alert(`You can't message Joinmultiverse right now. Sorry!`);
+});
